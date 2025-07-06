29d05e7995cc2a36a638cd2bcfba5d9f00a532c6
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-06 19:43:10 +0545

    model update

--- server/models/furniture.js
+++ server/models/furniture.js
@@ -1,53 +1,51 @@
 const mongoose = require("mongoose");
 
-const furniture = new mongoose.Schema({
+const furnitureSchema = new mongoose.Schema({
   name: String,
-  description: String,
+
+  description: {
+    summary: { type: String },
+    description: { type: String },
+   features: [{ type: String }],    
+   whatIncluded: [{ type: String }], 
+  },
+
   price: Number,
-    thumbnail: {
-    type: String,  
+
+  thumbnail: {
+    type: String,
     required: false,
   },
+
   colorOptions: [
     {
-      color: {
-        type: String,
-        required: true,
-      },
-      colorCode: {
-        type: String, 
-        required: false,
-      },
-      furnitureimages: {
-        type: [String], 
-        required: true,
-      }
-    }
+      color: { type: String, required: true },
+      colorCode: { type: String, required: false },
+      furnitureimages: { type: [String], required: true },
+    },
   ],
+
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
     required: true,
   },
+
   sector: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Sector",
     required: true,
   },
+
   productOverview: [
     {
-      icon: {
-        type: String,
-        required: false,
-      },
-      label: {
-        type: String,
-        required: true,
-      },
+      icon: { type: String, required: false },
+      label: { type: String, required: true },
     },
   ],
 
   specifications: {
+    specificationImage: { type: String }, 
     dimensions: {
       overall: String,
       overallProductWeight: String,
@@ -58,13 +56,14 @@ const furniture = new mongoose.Schema({
         },
       ],
     },
-    features: [
+    details: [ 
       {
         label: { type: String, required: true },
         value: { type: String, required: true },
       },
     ],
   },
+
   returnPolicy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "ReturnPolicy",
@@ -73,9 +72,9 @@ const furniture = new mongoose.Schema({
 
   tags: {
     type: [String],
-    enum: ['New Arrival', 'Best Seller', 'Featured', 'Popular', 'Recommended'],
+    enum: ["New Arrival", "Best Seller", "Featured", "Popular", "Recommended"],
     default: [],
   },
 });
 
-module.exports = mongoose.model("Furniture", furniture);
+module.exports = mongoose.model("Furniture", furnitureSchema);
