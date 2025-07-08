bb358e14de153a7fd9ceded4c81a3574728b4959
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-09 01:21:15 +0545

    changes

--- server/validation/authvalidation.js
+++ server/validation/authvalidation.js
@@ -4,7 +4,6 @@ const Joi = require('joi');
 const AuthValidationSchema = Joi.object({
   username: Joi.string().required().min(3).max(255), 
   email: Joi.string().required().email(), 
-
   role: Joi.string().valid('user', 'admin').default('user'), 
 });
 
