0e97b825462c304dae1262bfdf2df68e161d789f
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-09 08:19:27 +0545

    cart context update

--- frontend_styelo/src/context/cartcontext.jsx
+++ frontend_styelo/src/context/cartcontext.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
-import { useAuth } from "./authconetxt"; // ensure this path is correct
+import { useAuth } from "./authconetxt"; 
 
 const CartContext = createContext();
 
