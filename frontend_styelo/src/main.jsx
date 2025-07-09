585e9ff20d846d29e695178d61585701d9739b75
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-09 08:23:48 +0545

    glb file

--- frontend_styelo/src/main.jsx
+++ frontend_styelo/src/main.jsx
@@ -13,7 +13,6 @@ createRoot(document.getElementById("root")).render(
       <WishlistProvider>
         <CartProvider>
           <OrderProvider>
-
         <App />
           </OrderProvider>
         </CartProvider>
