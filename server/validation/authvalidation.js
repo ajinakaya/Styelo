c92f8d8f05d1b579d332e10da80bea3a6be04aaa
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-09 07:18:51 +0545

    update

--- server/validation/authvalidation.js
+++ server/validation/authvalidation.js
@@ -4,14 +4,14 @@ const Joi = require('joi');
 const AuthValidationSchema = Joi.object({
   username: Joi.string().required().min(3).max(255), 
   email: Joi.string().required().email(), 
-  password: Joi.string().required().min(8).max(255), 
+
   role: Joi.string().valid('user', 'admin').default('user'), 
 });
 
 function AuthValidation(req, res, next) {
-  const {  username, email, password, role } = req.body;
+  const {  username, email,  role } = req.body;
   
-  const { error } = AuthValidationSchema.validate({  username, email, password, role });
+  const { error } = AuthValidationSchema.validate({ username, email, role });
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
